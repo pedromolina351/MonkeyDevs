fc8d7fa286f6e54ba677210a3333c54ae3bdbe14
Williams Ramos <williams14cc@gmail.com>
2023-05-06 17:36:48 -0600

    Perfil y Landing Modificados

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
 
+  imagenes:any[]=[
+    {
+      name:'imagenHTML',
+      img: '../../assets/img/ImagenHTML.jfif',
+      desc: 'Programa en HTML'
+    },
+    {
+      name:'imagenCSS',
+      img: '../../assets/img/ImagenCSS.jpg',
+      desc: 'Programa en CSS'
+    },
+    {
+      name:'imagenJS',
+      img: '../../assets/img/ImagenJS.jpg',
+      desc: 'Programa en JavaScript'
+    },
+  ];
+
+  constructor(private _config:NgbCarouselConfig){
+    _config.interval = 2000;
+  }
 }
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.services';
 
 @Component({
   selector: 'app-perfil',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  nombre = '';
+  apellido = '';
+  nombreUsuario = '';
+  correoElectronico = '';
 
+  constructor(
+    private authService: AuthService
+    ){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const nombreTokens = this.authService.getUserData();
+    const apellidoTokens = this.authService.getUserData();
+    const nombreUsuarioTokens = this.authService.getUserData();
+    const correoElectronico = this.authService.getUserData();
+
+    this.nombre = nombreTokens.nombre;
+    this.apellido = apellidoTokens.apellido;
+    this.nombreUsuario = nombreUsuarioTokens.nombreUsuario;
+    this.correoElectronico = correoElectronico.correoElectronico;
+  }
+
+  toggleDiv() {
+    const div2:any = document.getElementById("div-2");
+    div2.style.display = "block";
+  }
+  
+  closeDiv(): void {
+    const div2:any = document.getElementById("div-2");
+    div2.style.display = "none";
+  }
+  
+  
 }
